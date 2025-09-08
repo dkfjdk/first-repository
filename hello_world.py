c03d807df538b32411a9b007e591d160df646e6b
dkfjdk <rjsdlf1794@gmail.com>
2025-09-08 14:38:08 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,10 @@
 def print_hello():
     print("Hello World!")
 
+def print KIT():
+    print("Kumoh National Institute of Technology.")
+
 if_name_ == '_main_':
+    print_KIT()
     print_hello()
+    
